20ef3b1553f7a2d5ed64ff397126a28fb6450a59
Jakub <staniojakub@gmail.com>
2019-11-08 22:27:38 +0100

    Add CRUD API for words table

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
 const express = require('express')
+const { getAll, getOne, save, remove } = require('./repository/words')
 
 const app = express()
 const PORT = 3000
+
+// Enable json parser
+app.use(express.json())
+
+// Get all words
+app.get('/api/words', (req, res) => {
+  getAll()
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+})
+
+// Get specific word
+app.get('/api/words/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  getOne(id)
+    .then(word => {
+      res.send(word)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(404).send()
+    })
+})
+
+// Add new word
+app.post('/api/words', (req, res) => {
+  const { word } = req.body
+
+  if (!word) {
+    throw Error('Missing `word` attribute')
+  }
+
+  save(word)
+    .then(addedWord => res.send(addedWord))
+})
+
+// Remove a word
+app.delete('/api/words/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  remove(id)
+    .then(_ => {
+      res.status(204).send()
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+})
 
 app.listen(PORT, () => console.log(`App running on port: ${PORT}`))
---
+++ repository/words.js
@@ -0,0 +1,69 @@
+const sqlite3 = require('sqlite3').verbose()
+
+const db = new sqlite3.Database('./test.db', (err) => {
+  if (err) {
+    console.error(err.message)
+  }
+  console.log('Connected to the database')
+})
+
+// Fetch all words
+const getAll = () => new Promise((resolve, reject) => {
+  db.all('SELECT * FROM words', (err, rows) => {
+    if (err) {
+      reject(err)
+    }
+    resolve(rows)
+  })
+})
+
+// Fetch one word
+const getOne = (id) => new Promise((resolve, reject) => {
+  db.all('SELECT * FROM words WHERE id=$id', [id], (err, rows) => {
+    if (err) {
+      reject(err)
+    }
+    if (rows.length > 0) {
+      resolve(rows[0])
+    } else {
+      reject(new Error('not Found'))
+    }
+  })
+})
+
+// Add word
+const save = (word) => new Promise((resolve, reject) => {
+  db.run(
+    'INSERT INTO words VALUES (null, $word)',
+    [word],
+    function (err) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ id: this.lastID, word: word })
+      }
+    }
+  )
+})
+
+// Remove word of provided id
+const remove = (id) => new Promise((resolve, reject) => {
+  db.run(
+    'DELETE FROM words WHERE id=$id',
+    [id],
+    function (err) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    }
+  )
+})
+
+module.exports = {
+  getAll,
+  getOne,
+  save,
+  remove
+}
