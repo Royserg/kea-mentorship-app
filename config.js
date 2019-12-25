d5f459bc708c65877418e3191b9f101e10950739
Jakub <staniojakub@gmail.com>
2019-12-25 15:34:25 +0100

    Update config file

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 const config = {
-  APP_PORT: 5000,
+  SERVER_PORT: 5000,
   DB_USERNAME: 'mentor',
   DB_PASSWORD: 'mentorPass',
-  DB_NAME: 'mentorship'
+  DB_NAME: 'mentorship',
+
+  TOKEN_SECRET: 'My4escret4JWT*@!2.,,,',
+  TOKEN_EXPIRATION: '1h'
 }
 
 module.exports = config
